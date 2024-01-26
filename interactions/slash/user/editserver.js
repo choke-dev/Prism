ccad2bf364f8130706b896fa5fe777edde7e00fc
choke-dev <47597850+choke-dev@users.noreply.github.com>
2024-01-26 20:57:25 +0800

    Revert "a"
    This reverts commit b023938b2870276587238b7033a27d9bdd956a0d.

--- interactions/slash/user/editserver.js
+++ interactions/slash/user/editserver.js
@@ -16,19 +16,64 @@ module.exports = {
 	// The data needed to register slash commands to Discord.
 
 	data: new SlashCommandBuilder()
-		.setName("wiki")
+		.setName("editserver")
 		.setDescription(
-			"Returns a wiki article"
+			"Edit something about the server."
 		)
 		.addStringOption((option) =>
 	    option
-	      .setName("article_page")
-	      .setDescription("The article page in the wiki")
+	      .setName("name")
+	      .setDescription("Edits the server name")
+	      .setRequired(false)
+		)
+		.addAttachmentOption((option) =>
+	    option
+	      .setName("icon")
+	      .setDescription("Edits the server icon")
+	      .setRequired(false)
+		)
+		.addStringOption((option) =>
+	    option
+	      .setName("icon_link")
+	      .setDescription("Edits the server icon from the specified link")
+	      .setRequired(false)
 		),
 
 	async execute(interaction) {
-		const articlePage = interaction.options.getString("article_page");
+		const oldServerName = interaction.guild.name;
+		const newServerName = interaction.options?.getString("name");
+    	let newServerIcon = (interaction.options.getAttachment("icon")?.url || interaction.options?.getString("icon_link"));
 
-		return interaction.reply(`https://pillars.miraheze.org/wiki/${articlePage}`)
+    	const updateOptions = {
+			reason: `Edited by ${interaction.user.username} (${interaction.user.id})`
+		};
+		
+		await interaction.deferReply();
+
+    	if (newServerIcon) {
+    	    const iconResponse = await fetch(newServerIcon);
+    	    newServerIcon = await iconResponse.body
+    	    updateOptions.icon = newServerIcon;
+    	}
+
+    	if (newServerName) {
+    	    updateOptions.name = newServerName;
+    	}
+
+    	try {
+    	    await interaction.guild.edit(updateOptions);
+    	    let replyMessage = ':white_check_mark: Successfully';
+    	    if (newServerName && newServerIcon) {
+    	        replyMessage += ` changed the server icon and name from "${oldServerName}" to "${newServerName}"`;
+    	    } else if (newServerName) {
+    	        replyMessage += ` changed the server name from "${oldServerName}" to "${newServerName}"`;
+    	    } else if (newServerIcon) {
+    	        replyMessage += ' changed the server icon.';
+    	    }
+    	    return await interaction.editReply(replyMessage);
+    	} catch (error) {
+    	    console.error(error);
+    	    return await interaction.editReply(':x: Failed to update server properties.')
+    	}
 	},
 };
